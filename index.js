0b5d22fea5149c4512879d4bbd4511f4844a3616
MohammadNasir520 <nasirahsan520@gmail.com>
2022-11-09 00:19:11 +0600

    review edit done

--- index.js
+++ index.js
@@ -84,11 +84,35 @@ async function run() {
       res.send(reviews);
     });
 
-
+// delete review 
     app.delete('/reviews/:id',async(req, res)=>{
         const id = req.params.id;
         const query ={_id: ObjectId(id)}
         const result= await ReviwsCollection.deleteOne(query)
+        res.send(result)
+    })
+
+
+
+//get review by id
+    app.get('/reviews/:id',async(req, res)=>{
+        const id = req.params.id;
+        const query ={_id: ObjectId(id)}
+        const result= await ReviwsCollection.findOne(query)
+        res.send(result)
+    })
+
+    app.put('/reviews/:id',async(req, res)=>{
+        const id = req.params.id;
+        console.log(id)
+        const review=req.body.review;
+        const query ={_id: ObjectId(id)}
+        const updateDoc={
+            $set:{
+                review: review
+            }
+        }
+        const result= await ReviwsCollection.updateOne(query,updateDoc)
         res.send(result)
     })
 
