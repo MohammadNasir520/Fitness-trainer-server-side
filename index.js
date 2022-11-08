8f1b82b25744697f198d633456da71c3c53c35e5
MohammadNasir520 <nasirahsan520@gmail.com>
2022-11-08 18:28:07 +0600

    query my review

--- index.js
+++ index.js
@@ -1,102 +1,112 @@
-const express = require('express');
-const app= express();
-const cors = require('cors');
-const port=process.env.PORT || 5000;
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+require("dotenv").config();
 
 // midle ware
 app.use(cors());
 app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.c5dej4c.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-
-
-async function run(){
-
-    try{
-        // collection
-        const ServiceCollection=client.db('servicesDb').collection('services')
-
-        const ReviwsCollection=client.db('servicesDb').collection('Reviews')
-
-
-
-        // create servises by add services
-        app.post('/services', async(req, res)=>{
-           
-            const services =req.body;
-            const result= await ServiceCollection.insertOne(services);
-            res.send(result);
-            
-        })
-
-
-        //servicess get method for home page
-        app.get('/services',async(req,res)=>{
-            let query= {};
-            const cursor=ServiceCollection.find(query).limit(3)
-            const services= await cursor.toArray();
-            res.send(services)
-        }) 
-
-        // all services fr see all button
-        app.get('/allServices',async(req,res)=>{
-            let query= {};
-            const cursor=ServiceCollection.find(query)
-            const services= await cursor.toArray();
-            res.send(services)
-        })
-
-        // specificServiceDetails
-        app.get('/service/:id',async(req, res)=>{
-            const id = req.params.id;
-            const query ={_id: ObjectId(id)}
-            const result= await ServiceCollection.findOne(query)
-            res.send(result)
-        })
-
-        // add reviews
-        app.post('/reviews', async(req, res)=>{
-           
-            const reviews =req.body;
-            const result= await ReviwsCollection.insertOne(reviews);
-            res.send(result);
-            
-        })
-
-        // get all reviews .
-        app.get('/reviews',async(req,res)=>{
-            let query={}
-           if(req.query.serviceId){
-            query={
-                serviceId: req.query.serviceId
-            }
-           }
-            const cursor=ReviwsCollection.find(query)
-            const reviews= await cursor.toArray();
-            res.send(reviews)
-        })
-
-
-    }
-    finally{
-
-    }
-}
-run().catch(err=>console.log(err))
-
-
-
-app.get('/',(req, res)=>{
-    res.send('server api is running')
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
+async function run() {
+  try {
+    // collection
+    const ServiceCollection = client.db("servicesDb").collection("services");
 
-app.listen(port, ()=>{
-    console.log(`server is running on ${port}`)
-})
+    const ReviwsCollection = client.db("servicesDb").collection("Reviews");
+
+    // create servises by add services
+    app.post("/services", async (req, res) => {
+      const services = req.body;
+      const result = await ServiceCollection.insertOne(services);
+      res.send(result);
+    });
+
+    //servicess get method for home page
+    app.get("/services", async (req, res) => {
+      let query = {};
+      const cursor = ServiceCollection.find(query).limit(3);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    // all services fr see all button
+    app.get("/allServices", async (req, res) => {
+      let query = {};
+      const cursor = ServiceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    // specificServiceDetails
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await ServiceCollection.findOne(query);
+      res.send(result);
+    });
+
+    // add reviews
+    app.post("/reviews", async (req, res) => {
+      const reviews = req.body;
+      const result = await ReviwsCollection.insertOne(reviews);
+      res.send(result);
+    });
+
+    // query all reviews by service id .
+    app.get("/reviews", async (req, res) => {
+      console.log(req.query);
+
+      let query = {};
+
+      if (req.query.email) {
+        query = {
+          email: req.query.email,
+        };
+      }
+      if (req.query.serviceId) {
+        query = {
+          serviceId: req.query.serviceId,
+        };
+      }
+
+      const cursor = ReviwsCollection.find(query);
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    });
+
+    // // query all reviews by service user email .
+    // app.get("/reviews", async (req, res) => {
+    //   console.log(req.query.email);
+    //   let query = {};
+    //   if (req.query.email) {
+    //     query = {
+    //       email: req.query.email,
+    //     };
+       
+    //   }
+    //   const cursor = ReviwsCollection.find(query);
+    //   const reviews = await cursor.toArray();
+    //   res.send(reviews);
+    // });
+  } finally {
+  }
+}
+run().catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("server api is running");
+});
+
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
+});
